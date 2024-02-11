7dd6720dbadbc940c6fa15143a6622554a47fb74
Kai Liu <kailiu@Kais-MacBook-Air.local>
2024-02-11 10:51:04 -0600

    updating new repos

---
+++ Hangman.py
@@ -0,0 +1,14 @@
+#file for the hangman game
+from utils.valid import word_chooser
+from utils.wordlist import words
+from utils.visual import lives_visual_dict
+
+def hangman():
+    
+
+
+
+
+
+if __name__ == '__main__':
+    hangman()
